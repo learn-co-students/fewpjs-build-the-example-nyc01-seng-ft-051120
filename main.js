f8e62e909d77079b67be9243c7d915ba9e8d5dc7
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-06-28 18:59:48 -0400

    Done.

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+let errorModal = document.querySelector(".hidden");
 
 // Your JavaScript code goes here!
+document.body.addEventListener("click", e => {
+    if (!e.target.className.includes("activated-heart")) {      
+      mimicServerCall()
+      .then(changeHeart(e))
+      .catch(err => revealErrorModal(err, e))
+    } else {
+      emptyHeart(e)
+    }
+});
 
+function revealErrorModal(error, e) {
+  errorModal.classList.remove("hidden")
+  errorModal.innerText = error;
+  setTimeout(() => errorModal.className = "hidden", 5000);
+  emptyHeart(e);
+}
 
-
+function changeHeart(e) {
+  if(e.target.className.includes("activated-heart")) {
+    emptyHeart(e);
+  }
+  else if(e.target.className.includes("like-glyph")) {
+    fullHeart(e);
+  }
+}
+function emptyHeart(e) {
+  e.target.innerText = EMPTY_HEART;
+  e.target.classList.remove("activated-heart")
+}
+function fullHeart(e) {
+  e.target.innerText = FULL_HEART;
+  e.target.classList.add("activated-heart")
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
