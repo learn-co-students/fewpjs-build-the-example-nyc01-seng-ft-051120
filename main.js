a0110b871ac56597c3230714d2603b28aca2ac03
emotionalproof <chaserabenn@gmail.com>
2020-07-02 16:22:21 -0400

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,68 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function(e){
+  // hideError()
+  clickHandler()
+  const url = 'https://dog.ceo/api/breeds/image/random'
+})
+const errorModal = document.querySelector("div#modal")
 
+// const hideError = () => {
+//   errorModal.className = "hidden"
+// }
+
+const clickHandler = () => {
+  document.addEventListener("click", function(e){
+    const clicked = e.target
+    if(checkForHeart(clicked)){
+      heartClickHandler(e.target)        
+    }
+  })
+}
+
+const checkForHeart = clicked => {
+  const clickedClass = clicked.className
+  return (clickedClass === "like-glyph" || clickedClass === "activated-heart")
+}
+const heartClickHandler = (heart) => {
+  if (heart.textContent === EMPTY_HEART){
+    likeHandler(heart)
+  } else if (heart.textContent === FULL_HEART){
+    unlike(heart)
+  }
+}
+
+likeHandler = (heart) => {
+  mimicServerCall("bogusUrl")
+    .then(promise => {
+      heart.className = "activated-heart"
+      heart.textContent = FULL_HEART
+    })
+    .catch(error => {
+      errorHandler(error)
+    })
+}
+
+const unlike = heart => {
+  mimicServerCall("bogusUrl")
+    .then(promise => {
+      heart.textContent = EMPTY_HEART
+      heart.className = "like-glyph"
+    })
+    .catch(error => {
+      errorHandler(error)
+    })
+}
+
+const errorHandler = (error) => {
+  const p = errorModal.lastChild
+  p.textContent = error
+  errorModal.className = ""
+  window.setTimeout(() => {
+    errorModal.className = "hidden"
+  }, 5 * 1000)
+}
 
 
 
